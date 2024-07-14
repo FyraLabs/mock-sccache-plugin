cad37f696a6335eee445d1e8c27c42073fbf4c81
june-fish <git@june.fish>
2024-07-14 09:52:14 -0700

    init commit

---
+++ sccache.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# vim:expandtab:autoindent:tabstop=4:shiftwidth=4:filetype=python:textwidth=0:
+# License: GPL2 or later see COPYING
+# Written by Michael Brown
+# Copyright (C) 2007 Michael E Brown <mebrown@michaels-house.net>
+
+# python library imports
+
+# our imports
+from mockbuild.mounts import BindMountPoint
+from mockbuild.trace_decorator import getLog, traceLog
+from mockbuild import file_util
+
+requires_api_version = "1.1"
+run_in_bootstrap = False
+
+# plugin entry point
+@traceLog()
+def init(plugins, conf, buildroot):
+    SCCache(plugins, conf, buildroot)
+
+
+class SCCache(object):
+    """enables sccache in buildroot/rpmbuild"""
+    # pylint: disable=too-few-public-methods
+    @traceLog()
+    def __init__(self, plugins, conf, buildroot):
+        self.buildroot = buildroot
+        self.config = buildroot.config
+        self.state = buildroot.state
+        self.sccache_opts = conf
+        tmpdict = self.sccache_opts.copy()
+        tmpdict.update({'chrootuid': self.buildroot.chrootuid})
+        self.sccachePath = self.sccache_opts['dir'] % tmpdict
+        buildroot.preexisting_deps.append("sccache")
+        plugins.add_hook("prebuild", self._sccacheBuildHook)
+        plugins.add_hook("preinit", self._sccachePreInitHook)
+        buildroot.mounts.add(
+            BindMountPoint(srcpath=self.sccachePath, bindpath=buildroot.make_chroot_path("/var/tmp/ccache")))
+
+    # =============
+    # 'Private' API
+    # =============
+    # set the max size before we actually use it during a build. ccache itself
+    # manages size and settings. we also set a few variables used by ccache to
+    # find the shared cache.
+    @traceLog()
+    def _sccacheBuildHook(self):
+        self.buildroot.doChroot("sccache", "--start-server", shell=False)
+
+    # set up the sccache dir.
+    # we also set a few variables used by sccache to find the shared cache.
+    @traceLog()
+    def _sccachePreInitHook(self):
+        getLog().info("enabled sccache")
+        envupd = {"SCCACHE_DIR": "/var/tmp/sccache",
+            "SCCACHE_CACHE_SIZE": self.sccache_opts['max_cache_size'],
+            "RUSTC_WRAPPER": "/usr/bin/sccache"}
+        self.buildroot.env.update(envupd)
+
+        file_util.mkdirIfAbsent(self.buildroot.make_chroot_path('/var/tmp/sccache'))
+        file_util.mkdirIfAbsent(self.sccachePath)
+        self.buildroot.uid_manager.changeOwner(self.sccachePath, recursive=True)
